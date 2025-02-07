323fb0bb8b8d69f1a8c60a168f9bede2190f0be9
Sila <s.ordu@imperial.ac.uk>
2025-02-07 09:23:49 +0000

    w4

--- Demo_W4.sol
+++ Demo_W4.sol
@@ -10,19 +10,27 @@ contract Escrow {
 
     // Constructor should the escrow contract with the payer, payee, and agreed amount
     constructor() {
-
+        payer = _payer;
+        payee = _payee;
+        lawyer = _lawyer;
+        amount = _amount;
     }
 
     // This function should allow the payer to deposit Ether into the escrow contract
     // It requires the caller to be the payer and ensures deposits don't exceed the agreed amount
     // Ether is sent by attaching a `msg.value` with the function call
 
-    function deposit()  {
+    function deposit() public payable {
+        require(msg.sender == payer, "Only payer can deposit");
+        require(address(this).balance <= amount, "Cannot exceed agreed amount");
     }
 
     // This function allows the lawyer to release funds to the payee when conditions are met
     // It can only be called by the lawyer and requires the contract balance to match the agreed amount
     function release() public { 
+        require(msg.sender == lawyer, "Only lawyer can release funds");
+        require(address(this).balance == amount, "Contract balance must match agreed amount");
+    
         payable(payee).transfer(amount);  // The payable modifier allows the address to transfer of Ether
     }
 
